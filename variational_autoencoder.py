d22c803439e17ea6b3467a523265489ae6e9914a
WarrenGreen <s.green.warren@gmail.com>
2019-01-06 17:02:57 -0800

    fix loss

--- variational_autoencoder.py
+++ variational_autoencoder.py
@@ -28,6 +28,7 @@ from scipy.stats import norm
 import tensorflow as tf
 
 # Import MNIST data
+from tensorflow.contrib.losses import sigmoid_cross_entropy
 from tensorflow.examples.tutorials.mnist import input_data
 from tensorflow.python.layers.core import Flatten
 
@@ -89,11 +90,7 @@ output = tf.layers.Conv2D(image_channels, (2,2), activation=tf.sigmoid, padding=
 # Define VAE Loss
 def vae_loss(x_reconstructed, x_true):
     # Reconstruction loss
-    x_true = tf.reshape(x_true, (-1, 784))
-    x_reconstructed = tf.reshape(x_reconstructed, (-1, 784))
-    encode_decode_loss = x_true * tf.log(1e-10 + x_reconstructed) \
-                         + (1 - x_true) * tf.log(1e-10 + 1 - x_reconstructed)
-    encode_decode_loss = -tf.reduce_sum(encode_decode_loss, 1)
+    encode_decode_loss = image_dim*image_dim*image_channels * sigmoid_cross_entropy(x_reconstructed, x_true)
     # KL Divergence loss
     kl_div_loss = 1 + z_std - tf.square(z_mean) - tf.exp(z_std)
     kl_div_loss = -0.5 * tf.reduce_sum(kl_div_loss, 1)
